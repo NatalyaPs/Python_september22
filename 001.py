c7beebf1ef43191453046a756a484fb699f93c0b
NatalyaPS <n09051@bk.ru>
2022-10-16 20:41:56 +0300

    добав повторно 001 и лекц 3

--- 001.py
+++ 001.py
@@ -1,8 +1,6 @@
 # a = [1,2,3,4,5]
 # x = sum([a[i] for i in range(1, len(a), 2)])
 # print(x)
-
-
-print(2021%28)
-print(28*4)
-print(117%28)
+# 
+list = [i for i in range(1, 11) if i%2 ==0] # [2,4,6,8,10]
+print(list)
